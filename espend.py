5afe87055fd4487562e9f326376dd0134dfa80a0
ruchinthakaishan <ruchinthakaishanktg@gmail.com>
2026-07-08 13:59:50 +0530

    Update espend.py

--- espend.py
+++ espend.py
@@ -1,1 +1,7 @@
-print("**********************It works****************")
+def espend(app, request, jsonify):
+    print("********************It works****************")
+
+    return jsonify({
+        "success": False,
+        "message": "Remote espend.py is running"
+    }), 503
